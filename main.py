2b4bab385a56d29a9c3f3259529919dd0f588677
Miguel Lopez <ing.miguell@gmail.com>
2023-06-13 14:34:19 -0600

    delete credentials

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import requests
+from datetime import datetime
 
-APP_ID = "a37fbeeb"
-API_KEY = "728a5146763a516a57a769adfd5d8cc5"
+APP_ID = ""
+API_KEY = ""
 BASE_URL = "https://trackapi.nutritionix.com/"
+
+SHEETY_URL = "https://api.sheety.co/fe3dcca35ca1d03f184574cef51bbcd6/workoutTracking/workouts"
 
 HEADERS = {
     "x-app-id": APP_ID,
     "x-app-key": API_KEY,
     "x-remote-user-id": "0"
+}
+
+TOKEN = {
+    "Authorization": ""
 }
 
 exercise_data = {
@@ -22,4 +29,19 @@ response = requests.post(url=f"{BASE_URL}v2/natural/exercise", json=exercise_dat
 response.raise_for_status()
 data = response.json()
 
-print(data)
+today = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+for exercise in data["exercises"]:
+    body = {
+        "workout": {
+            "date": today,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+    response_sheety = requests.post(url=SHEETY_URL, json=body, headers=TOKEN)
+    response_sheety.raise_for_status()
+
+print(response_sheety)
